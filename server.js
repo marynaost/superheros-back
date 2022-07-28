4ac211e771b815db47424f3d63a21e36b9ec4374
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-07-28 10:27:26 +0300

    fix mongoose

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST } = process.env;
+// const { DB_HOST } = process.env;
 
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
     console.log("Database connection successful");
     app.listen(3000);
