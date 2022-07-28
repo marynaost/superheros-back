a6b56b956a2fcbcfbe65fcaae555b6bf3e27b728
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-07-28 11:08:32 +0300

    fix server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-// const { DB_HOST } = process.env;
+const { DB_HOST } = process.env;
 
 mongoose
-  .connect(
-    "mongodb+srv://Maryna:01111995nm@cluster0.avjyq.mongodb.net/superheroes?retryWrites=true&w=majority"
-  )
+  .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
     app.listen(3000);
