51943766e6da77db9ff35eaea68a2223f7314608
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-08-01 15:52:59 +0300

    some fix in shemes

--- models/schemes.js
+++ models/schemes.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 
 const heroesSchema = Joi.object({
-  nickname: Joi.string().min(2).max(50).required(),
-  real_name: Joi.string().min(2).max(50),
-  origin_description: Joi.string().min(2).max(1000),
-  superpowers: Joi.string(),
-  catch_phrase: Joi.string(),
-  images: Joi.string().min(2).max(200),
+  nickname: Joi.string().min(2).max(40),
+  real_name: Joi.string().min(2).max(40),
+  origin_description: Joi.string().min(2).max(600),
+  superpowers: Joi.string().min(2).max(550),
+  catch_phrase: Joi.string().min(2).max(200),
+  // images: Joi.string(),
 
   // images: Joi.array().items(Joi.string()),
 });
